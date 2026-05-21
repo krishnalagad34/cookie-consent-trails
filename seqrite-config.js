00b7bcb0d384e0a95c352347445f81fbe01715ff
krishnalagad34 <krishna.lagad@quickheal.com>
2026-05-21 18:49:13 +0530

    Adding js and css files

---
+++ seqrite-config.js
@@ -0,0 +1,299 @@
+
+      let userLang = (navigator.language || (navigator).userLanguage || 'en').split('-')[0];
+      console.log("Browser Language Code: ", userLang);
+      window.klaroConfig = {
+  "consentManagerId": "6a0ef1a8d467bc61793be075",
+  "uuid": "023d5b04-1ece-4afe-b26e-6d5a0a3923c9",
+  "domain": "http://localhost/cmapi",
+  "version": 1,
+  "elementID": "SEQRITE",
+  lang: userLang,
+  "storageMethod": "cookie",
+  "storageName": "https://precious-crisp-8d1b55.netlify.app/",
+  "cookieExpiresAfterDays": 365,
+  "default": true,
+  "mustConsent": false,
+  "acceptAll": true,
+  "hideDeclineAll": false,
+  "hideLearnMore": false,
+  "disablePoweredBy": false,
+  "translations": {
+    "english": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "OK",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "<p>Customize Consent Preferences</p>",
+        "description": "<p>We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website. However, we acknowledge your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site.</p>",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {},
+      "service": {
+        "disableAll": {
+          "description": "Reject All",
+          "title": "Reject All"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        }
+      },
+      "purposeItem": {
+        "service": "Cookie",
+        "services": "Cookies"
+      },
+      "contextualConsent": {
+        "description": "To show this content, we need your consent. Please accept the relevant cookies.",
+        "acceptOnce": "Accept once",
+        "acceptAlways": "Accept always"
+      },
+      "analytics": {
+        "title": "Analytics",
+        "description": "<p>NA</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;_ga</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Google Analytics</div></div></div></details>"
+      },
+      "marketing": {
+        "title": "Marketing",
+        "description": "<p>NA</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;_fbp</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Facebook/Meta Pixel</div></div></div></details>"
+      },
+      "others": {
+        "title": "Others",
+        "description": "<p>NA</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;session_id</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Other Cookies</div></div></div></details>"
+      }
+    },
+    "en": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "OK",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {},
+      "service": {
+        "disableAll": {
+          "description": "Reject All",
+          "title": "Reject All"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        }
+      },
+      "purposeItem": {
+        "service": "Cookie",
+        "services": "Cookies"
+      },
+      "contextualConsent": {
+        "description": "To show this content, we need your consent. Please accept the relevant cookies.",
+        "acceptOnce": "Accept once",
+        "acceptAlways": "Accept always"
+      },
+      "analytics": {
+        "title": "Analytics",
+        "description": "<p>NA</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;_ga</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Google Analytics</div></div></div></details>"
+      },
+      "marketing": {
+        "title": "Marketing",
+        "description": "<p>NA</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;_fbp</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Facebook/Meta Pixel</div></div></div></details>"
+      },
+      "others": {
+        "title": "Others",
+        "description": "<p>NA</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;session_id</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Other Cookies</div></div></div></details>"
+      }
+    },
+    "zz": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "OK",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {},
+      "service": {
+        "disableAll": {
+          "description": "Reject All",
+          "title": "Reject All"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        }
+      },
+      "purposeItem": {
+        "service": "Cookie",
+        "services": "Cookies"
+      },
+      "contextualConsent": {
+        "description": "To show this content, we need your consent. Please accept the relevant cookies.",
+        "acceptOnce": "Accept once",
+        "acceptAlways": "Accept always"
+      },
+      "analytics": {
+        "title": "Analytics",
+        "description": "<p>NA</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;_ga</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Google Analytics</div></div></div></details>"
+      },
+      "marketing": {
+        "title": "Marketing",
+        "description": "<p>NA</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;_fbp</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Facebook/Meta Pixel</div></div></div></details>"
+      },
+      "others": {
+        "title": "Others",
+        "description": "<p>NA</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;session_id</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Other Cookies</div></div></div></details>"
+      }
+    },
+    "mr": {
+      "acceptAll": "सर्व स्वीकारा",
+      "accept": "स्वीकारा",
+      "decline": "सर्व नाकारा",
+      "acceptSelected": "निवडलेले स्वीकारा",
+      "ok": "ठीक आहे",
+      "consentNotice": {
+        "description": "आम्ही तुमच्या गोपनीयतेचा आदर करतो आम्ही आवश्यक साइट कार्यक्षमता, तसेच विपणन, वैयक्तिकरण आणि विश्लेषण सक्षम करण्यासाठी कुकीज संचयित करतो. \"सर्व स्वीकारा\" वर क्लिक करून, तुम्ही आमच्या कुकीजच्या वापरास संमती देता. तुम्ही कधीही तुमचे सेटिंग बदलू शकता. कुकी धोरण.",
+        "learnMore": "सानुकूलित करा",
+        "changeDescription": " "
+      },
+      "save": "स्वीकारा",
+      "consentModal": {
+        "title": "संमतीची प्राधान्ये सानुकूलित करा",
+        "description": "पृष्ठांदरम्यान कार्यक्षमतेने नेव्हिगेट करण्यात, तुमची प्राधान्ये संचयित करण्यात आणि सामान्यतः वेबसाइटचा तुमचा अनुभव सुधारण्यात मदत करण्यासाठी आम्ही कुकीज वापरतो. तथापि, आम्ही तुमच्या गोपनीयतेच्या अधिकाराची कबुली देतो आणि म्हणूनच तुम्ही काही प्रकारच्या कुकीजला परवानगी न देणे निवडू शकता. अधिक जाणून घेण्यासाठी आणि आपली पूर्वनिर्धारित मांडणी बदलण्यासाठी विविध श्रेणी शीर्षकांवर क्लिक करा. कृपया लक्षात घ्या की विशिष्ट प्रकारच्या कुकीज अवरोधित केल्याने तुमच्या संकेतस्थळावरील अनुभवावर परिणाम होऊ शकतो.",
+        "buttons": {
+          "save": "स्वीकारा",
+          "acceptAll": "सर्व स्वीकारा",
+          "decline": "सर्व नाकारा"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {},
+      "service": {
+        "disableAll": {
+          "description": "सर्व नाकारा",
+          "title": "सर्व नाकारा"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        }
+      },
+      "purposeItem": {
+        "service": "Cookie",
+        "services": "Cookies"
+      },
+      "contextualConsent": {
+        "description": "To show this content, we need your consent. Please accept the relevant cookies.",
+        "acceptOnce": "Accept once",
+        "acceptAlways": "Accept always"
+      },
+      "analytics": {
+        "title": "Analytics",
+        "description": "एन. ए<details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;_ga</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Google Analytics</div></div></div></details>"
+      },
+      "marketing": {
+        "title": "Marketing",
+        "description": "एन. ए<details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;_fbp</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Facebook/Meta Pixel</div></div></div></details>"
+      },
+      "others": {
+        "title": "Others",
+        "description": "एन. ए<details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;session_id</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Other Cookies</div></div></div></details>"
+      }
+    }
+  },
+  "styling": {
+    "theme": [
+      "light",
+      "custom"
+    ]
+  },
+  "htmlTexts": true,
+  "embedded": false,
+  "declarative": false,
+  "groupByPurpose": false,
+  "hideToggleAll": false,
+  "appName": "KrishnaTest_1",
+  "description": "We respects your privacy and is committed to protecting your personal data.",
+  "companyName": "KrishnaTest_1",
+  "companyAddress": " ",
+  "services": [
+    {
+      "name": "analytics",
+      "title": "Analytics",
+      "description": "<p>NA</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;_ga</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Google Analytics</div></div></div></details>",
+      "purposes": [],
+      "required": false,
+      "default": true,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_ga"
+      ]
+    },
+    {
+      "name": "marketing",
+      "title": "Marketing",
+      "description": "<p>NA</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;_fbp</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Facebook/Meta Pixel</div></div></div></details>",
+      "purposes": [],
+      "required": false,
+      "default": true,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_fbp"
+      ]
+    },
+    {
+      "name": "others",
+      "title": "Others",
+      "description": "<p>NA</p><details style=\"margin-top:10px\"><summary style=\"cursor:pointer;font-weight:600;padding:2px 0\">1 Cookie</summary><div style=\"margin-top:6px;max-height:150px;overflow-y:auto\"><div style=\"padding:5px 0;border-bottom:1px solid rgba(0,0,0,0.07);font-size:12px;line-height:1.8\"><div><b>Name:</b>&nbsp;session_id</div><div><b>Host:</b>&nbsp;krishnalagad.github.io</div><div><b>Service:</b>&nbsp;Other Cookies</div></div></div></details>",
+      "purposes": [],
+      "required": false,
+      "default": true,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "session_id"
+      ]
+    }
+  ]
+};
+      showCookie();
